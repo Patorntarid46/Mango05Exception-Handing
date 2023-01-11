d960db01b9f1d7c2e375c55097d2cc9f9d5036aa
Patorntarid46 <118878463+Patorntarid46@users.noreply.github.com>
2023-01-11 15:00:17 +0700

    Add files via upload

---
+++ Mango05/src/ExceptionExample2.java
@@ -0,0 +1,14 @@
+class exceptionExample {
+    public static void main(String[] args) {
+
+        int a = 5, b = 0;
+
+        try {
+            int c = a / b;
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
